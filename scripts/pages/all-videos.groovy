50851e880d70813200bbebd48d62f8f60eb152f4
zac-poe <zachary.poe@rivetlogic.com>
2018-03-12 18:14:28 -0400

    adding retrieval of streams for search and all videos

--- scripts/pages/all-videos.groovy
+++ scripts/pages/all-videos.groovy
@@ -1,3 +1,3 @@
 templateModel.title = contentModel.title
-templateModel.q = 'content-type:"/component/video"'
+templateModel.q = 'content-type:"/component/video" OR (content-type:"/component/stream" AND endDate_dt:[NOW TO *])'
 templateModel.rows = contentModel.videoPageSize.text as int
