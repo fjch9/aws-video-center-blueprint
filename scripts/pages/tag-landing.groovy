a77485b0848252ddd2cf193f24970a5e72e5baf8
zac-poe <zachary.poe@rivetlogic.com>
2018-04-06 11:51:20 -0400

    adding support for tags to streams

--- scripts/pages/tag-landing.groovy
+++ scripts/pages/tag-landing.groovy
@@ -6,7 +6,7 @@ if (!tagId) {
 }
 
 templateModel.title = tagId
-templateModel.q = "content-type:\"/component/video\" AND tags.item.value_smv:\"${tagId}\""
+templateModel.q = "(content-type:\"/component/video\" OR content-type:\"/component/stream\") AND tags.item.value_smv:\"${tagId}\""
 templateModel.rows = contentModel.videoPageSize.text as int
 templateModel.breadcrumbs = [
 	[url: "/", label: "Home"],
